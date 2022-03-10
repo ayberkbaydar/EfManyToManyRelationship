07521ca2250ca47629342ddaab8151543bd47955
Ayberk <ybrk01@gmail.com>
2022-03-10 11:48:25 +0300

    10-03
    10-03

---
+++ ManyToManyRelationship/CodeFirst/EmployeeDBContext.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace CodeFirst
+{
+    public class EmployeeDBContext : DbContext
+    {
+        public DbSet<Course> Courses { get; set; }
+        public DbSet<Student> Students { get; set; }
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Student>()//FluentAPI
+            .HasMany(t => t.Courses)
+            .WithMany(t => t.Students)
+            .Map(m =>
+            {
+                m.ToTable("StudentCourses");
+                m.MapLeftKey("StudentID");
+                m.MapRightKey("CourseID");
+            });
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ ManyToManyRelationship/DatabaseFirst/WebForm1.aspx.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace DatabaseFirst
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+            EmployeeDBContext employeeDBContext = new EmployeeDBContext();
+            GridView1.DataSource = (from student in employeeDBContext.Students
+                                    from course in student.Courses
+                                    select new
+                                    {
+                                        StudentName = student.StudentName,
+                                        CourseName = course.CourseName
+                                    }).ToList();
+            GridView1.DataBind();
+        }
+    }
+}
